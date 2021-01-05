dc552cfe8238b4db00751bdca3fbd5306e897b5f
wang-boss <1002726239@qq.com>
2021-01-05 10:16:30 +0800

    modify router.js

--- src/router.js
+++ src/router.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: whf
+ * @Date: 2020-12-16 10:57:08
+ * @LastEditTime: 2021-01-05 10:15:52
+ * @FilePath: \WebDemo\src\router.js
+ */
 import Vue from "vue";
 import Router from "vue-router";
 import Index from "./views/Index.vue";
@@ -5,7 +11,6 @@ import Index from "./views/Index.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
