56b656e0cd28aca7aee810df07c210a28559a781
wang-boss <1002726239@qq.com>
2021-01-18 08:47:55 +0800

    fix the local resource cannot be accessed bug

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Letmeouted
+ * @Email: 1002726239@qq.com
+ * @Date: 2020-12-16 10:57:08
+ * @LastEditTime: 2021-01-18 08:39:25
+ * @FilePath: \WebDemo\vue.config.js
+ */
+module.exports = {
+    publicPath: './' // 开发环境相对路径
+}
